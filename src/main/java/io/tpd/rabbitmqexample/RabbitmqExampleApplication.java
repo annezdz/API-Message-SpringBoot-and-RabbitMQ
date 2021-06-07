13512e50c52929d8ff11c33998e792d5cd816340
annezdz <annezdz@hotmail.com>
2021-06-07 19:57:46 -0300

    Create PracticalTipMessage and Beans in RabbitmqExampleApplication

--- src/main/java/io/tpd/rabbitmqexample/RabbitmqExampleApplication.java
+++ src/main/java/io/tpd/rabbitmqexample/RabbitmqExampleApplication.java
@@ -1,13 +1,36 @@
 package io.tpd.rabbitmqexample;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RabbitmqExampleApplication {
+
+	private static final String EXCHANGE_NAME = "tips_tx";
+	private static final String DEFAULT_PARSING_QUEUE = "default_parser_q";
+	private static final String ROUTING_KEY = "tips";
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqExampleApplication.class, args);
 	}
 
+	@Bean
+	public TopicExchange tipsExchange(){
+		return new TopicExchange(EXCHANGE_NAME);
+	}
+
+	@Bean
+	public Queue defaultParsingQueue(){
+		return new Queue(DEFAULT_PARSING_QUEUE);
+	}
+
+	@Bean
+	public Binding queueExchangeBinding(){
+		return BindingBuilder.bind(defaultParsingQueue()).to(tipsExchange()).with(ROUTING_KEY);
+	}
 }
